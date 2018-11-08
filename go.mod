2f8ee39449bcdaf65eaa7d404d04902f4e463895
Clinton Bush <cbush06@gmail.com>
2018-11-07 20:55:17 -0500

    Tested named pipes for sending interrupts to intel8080 simulator

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/cbush06/spaceinvadersmachine
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"bitbucket.org/avd/go-ipc/fifo"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	sendInterrupt()
+	fmt.Println("Interrupt Sent...Exiting...")
+}
+
+func sendInterrupt() {
+	//interruptOpCode := []byte{0xEF}
+	interruptOpCode := []byte{0xAB}
+
+	interruptPipe, err := fifo.New("intel8080_interrupt", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("Unable to connect to named pipe [intel8080_interrupt]: " + err.Error())
+	}
+
+	defer interruptPipe.Close()
+
+	interruptPipe.Write(interruptOpCode)
 }
